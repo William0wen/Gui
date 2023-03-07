69bacae5fe140d14e10cc9683c2d9064eb853f9b
William Owen <98859927+William0wen@users.noreply.github.com>
2023-03-08 10:12:02 +1300

    comments

--- spelling_converter_v2.py
+++ spelling_converter_v2.py
@@ -2,10 +2,13 @@ import easygui
 
 repeat = "Yes"
 
+# main loop
 while repeat == "Yes":
     word = easygui.enterbox("Write a nz english word with an 'our', 'ise' or 'yse' in it: ")
     word_list = list(word)
 
+    # Checking the string to see if the options are in it
+    # and converting it to the american spelling
     if "our" in word:
         word_list.remove("u")
         easygui.msgbox(f"The american spelling of {word} is {''.join(word_list)}")
@@ -15,6 +18,7 @@ while repeat == "Yes":
     else:
         easygui.msgbox(f"No change in spelling for {word}")
 
+    # repeat?
     repeat = easygui.buttonbox("Do you want to convert another word to US english?", choices=["Yes", "No"])
 
 easygui.msgbox("Have a good day")
