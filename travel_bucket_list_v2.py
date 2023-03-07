d1609e3f7f80e075763538f71c720c81259685b6
William Owen <98859927+William0wen@users.noreply.github.com>
2023-03-08 10:15:41 +1300

    comments

--- travel_bucket_list_v2.py
+++ travel_bucket_list_v2.py
@@ -1,21 +1,27 @@
 import easygui
 
+# pre-setting variables
 places_list_ = []
 change_places = []
 place_to_remove = ""
 choice = "No"
 repeat = "Yes"
 
+# Loop to gather starting info
 while len(places_list_) != 5:
+    # enter five places
     places = easygui.enterbox("Enter the five places you want to visit"
                               "\neach separated by a comma only (no space)", "Favourite places")
+    # convert string into a list
     places_list_ = places.split(",")
     if len(places_list_) != 5:
         easygui.msgbox(f"Sorry, you must enter exactly 5 places and you entered {len(places_list_)} places", f"{len(places_list_)}/5 places")
 
+# Main loop
 while choice == "No":
     repeat = "Yes"
     output = "\n*  ".join(places_list_)
+    # print list
     easygui.msgbox("My bucket list:"
                    f"\n\n* {output}", "Bucket list")
 
@@ -24,6 +30,7 @@ while choice == "No":
         easygui.msgbox("Ok.")
     else:
         while repeat == "Yes":
+            # replace one string with another
             change = easygui.enterbox("Enter the name of the place you want to change followed by the new place"
                                       "\neach separated by a comma only (no space)", "Change Place")
             change_places = change.split(",")
